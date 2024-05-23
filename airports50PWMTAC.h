d8e9fe1a3e9a17ea09f2fed3f13a9fb59cf03816
alliefm <github@allie.fm>
2024-05-22 17:27:51 -0700

    Create airports50PWMTAC.h

---
+++ airports50PWMTAC.h
@@ -0,0 +1,54 @@
+#define NUM_AIRPORTS 50 // This is the number of entries in the airports.h file, including any NULL or KEY entries
+
+std::vector<String> airports({
+  "LIFR", // 1 order of LEDs, starting with 1 should be KKIC; use VFR, WVFR, MVFR, IFR, LIFR for key; NULL for no airport
+  "IFR", // 2
+  "MVFR", // 3
+  "WVFR", // 4
+  "VFR", // 5
+  "NULL", // 6
+  "KRKD", // 7
+  "NULL", // 8
+  "KBHB", // 9
+  "KBGR", // 10
+  "NULL", // 11
+  "KWVL", // 12
+  "KAUG", // 13
+  "KIWI", // 14
+  "KLEW", // 15
+  "KPWM", // 16
+  "KSFM", // 17
+  "KPSM", // 18
+  "KDAW", // 19
+  "KMHT", // 20
+  "KASH", // 21
+  "KCON", // 22
+  "KLCI", // 23
+  "KIZG", // 24
+  "NULL", // 25
+  "K8B0", // 26
+  "NULL", // 27
+  "KBML", // 28
+  "KHIE", // 29
+  "KCDA", // 30
+  "KEFK", // 31
+  "NULL", // 32
+  "KFSO", // 33
+  "KBTV", // 34
+  "KMPV", // 35
+  "NULL", // 36
+  "K1P1", // 37
+  "KLEB", // 38
+  "KRUT", // 39
+  "KVSF", // 40
+  "KEEN", // 41
+  "KAFW", // 42
+  "NULL", // 43
+  "NULL", // 44
+  "NULL", // 45
+  "NULL", // 46
+  "NULL", // 47
+  "NULL", // 48
+  "NULL", // 49
+  "NULL" // 50
+});
